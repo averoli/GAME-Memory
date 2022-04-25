98642bdab0aba78520c395da5b91800f56d8ac7f
Averoli <afanasenkavolha@gmail.com>
2022-04-25 16:19:06 +0200

    for pull

--- scripts/login.js
+++ scripts/login.js
@@ -10,44 +10,45 @@ btnLogIn.addEventListener("click", function BtnLogIn() {
     totalScore: 0,
   };
 
+  //   localStorage.setItem(up1.name, JSON.stringify(up1));
+  //   let data = JSON.parse(localStorage.getItem(up1.name))
 
-  localStorage.setItem(up1.name, JSON.stringify(up1));
-  let data = JSON.parse(localStorage.getItem(up1.name))
+  //     let player = document.createElement("li");
+  //     player.textContent = data.name;
+  //     document.getElementById("userList").appendChild(player);
 
-    let player = document.createElement("li");
-    player.textContent = data.name;
-    document.getElementById("userList").appendChild(player);
+  playerList.push(up1);
 
-//   playerList.push(up1);
+  localStorage.setItem("all_users", JSON.stringify(playerList));
 
-//   localStorage.setItem("all_users", JSON.stringify(playerList));
+  playerList = JSON.parse(localStorage.getItem("all_users"));
 
-//   playerList = JSON.parse(localStorage.getItem("all_users"));
+  localStorage.setItem("all_users", JSON.stringify(playerList));
 
-//   localStorage.setItem("all_users", JSON.stringify(playerList));
+  for (let i = 0; i < playerList.length; i++) {
+    if (playerList[i] !== up1) {
+        console.log(playerList[i]);
+        console.log(up1.name);
+      let player = document.createElement("li");
+      player.textContent = playerList[i].name;
+      document.getElementById("userList").appendChild(player);
+    } else {
+        console.log("kuku");
+    }
 
-//   for (let i = 0; i < playerList.length; i++) {
-//       let player = document.createElement("li");
-//       player.textContent = playerList[i].name;
-//       if( playerList[i] !== player.textContent){
-//           console.log(playerList[i])
-//           console.log(player.textContent)
+  
+  }
 
-//         document.getElementById("userList").appendChild(player);
-//       }
-      
-//   }
+  // IVAN
+  // ========================
+  // let listString= "";
+  // for (const player of playerList) {
 
-// IVAN
-// ========================
-// let listString= "";
-// for (const player of playerList) {
-
-//     let listElement = document.createElement("li");
-//     listElement.textContent = player.name;
-//     listString += listElement;
-// }
-// console.log(listString);
-// document.getElementById("userList").innerHTML = listString;
-//   inputUser.value = "";
+  //     let listElement = document.createElement("li");
+  //     listElement.textContent = player.name;
+  //     listString += listElement;
+  // }
+  // console.log(listString);
+  // document.getElementById("userList").innerHTML = listString;
+  //   inputUser.value = "";
 });
