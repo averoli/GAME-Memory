50096c6d84308ef607cae0690d324e781a9302cd
reduansj <reduansenoun@gmail.com>
2022-04-25 16:16:49 +0200

    feature ranking list

--- scripts/login.js
+++ scripts/login.js
@@ -2,52 +2,27 @@ const btnLogIn = document.getElementById("btnLogIn");
 const inputUser = document.getElementById("userNameImput");
 
 let playerList = [];
-let up1 = new Object();
+let userData = new Object();
 
 btnLogIn.addEventListener("click", function BtnLogIn() {
-  up1 = {
-    name: inputUser.value,
-    totalScore: 0,
-  };
+  if (inputUser.value !== "") {
+    userData = {
+      name: inputUser.value,
+      totalScore: 0,
+    };
+    playerList.push(userData);
+  }
 
+  localStorage.setItem("all_users", JSON.stringify(playerList));
+  let data = localStorage.getItem("all_users");
+  playerList = JSON.parse(data);
 
-  localStorage.setItem(up1.name, JSON.stringify(up1));
-  let data = JSON.parse(localStorage.getItem(up1.name))
-
-    let player = document.createElement("li");
-    player.textContent = data.name;
-    document.getElementById("userList").appendChild(player);
-
-//   playerList.push(up1);
-
-//   localStorage.setItem("all_users", JSON.stringify(playerList));
-
-//   playerList = JSON.parse(localStorage.getItem("all_users"));
-
-//   localStorage.setItem("all_users", JSON.stringify(playerList));
-
-//   for (let i = 0; i < playerList.length; i++) {
-//       let player = document.createElement("li");
-//       player.textContent = playerList[i].name;
-//       if( playerList[i] !== player.textContent){
-//           console.log(playerList[i])
-//           console.log(player.textContent)
-
-//         document.getElementById("userList").appendChild(player);
-//       }
-      
-//   }
-
-// IVAN
-// ========================
-// let listString= "";
-// for (const player of playerList) {
-
-//     let listElement = document.createElement("li");
-//     listElement.textContent = player.name;
-//     listString += listElement;
-// }
-// console.log(listString);
-// document.getElementById("userList").innerHTML = listString;
-//   inputUser.value = "";
+  if (playerList.length > 0) {
+    let actualPlayer = playerList.length - 1;
+    let listElement = document.createElement("li");
+    listElement.textContent = playerList[actualPlayer].name;
+    document.getElementById("userList").appendChild(listElement);
+  }
 });
+
+
