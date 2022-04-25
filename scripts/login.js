b756e36d92471e08db5594de39ea86e90e334a27
Averoli <afanasenkavolha@gmail.com>
2022-04-25 11:18:44 +0200

    for pull

--- scripts/login.js
+++ scripts/login.js
@@ -1,36 +1,30 @@
 const btnLogIn = document.getElementById("btnLogIn");
 const inputUser = document.getElementById("userNameImput");
 
+const DATA = localStorage.setItem("players", '[{name: "Volha", totalScore: 0}]')
+
+const data = JSON.parse(DATA)
+const allPlayers = document.querySelector(".players-list");
+
 let userName = "";
 
-const player = { name: "", totalScore: 0 };
-let players;
-
 btnLogIn.addEventListener("click", function BtnLog() {
-if(localStorage.name == ""){
-    console.log("1");
-    userName = inputUser.value;
-    (player.name = userName), (player.totalScore = 0);
-  
-    players = JSON.stringify(player);
-    localStorage.setItem("name", players);
-    inputUser.value = "";
-} else {
-    console.log("gaga");
-}
- 
+userName = inputUser.value;
 
- 
-  
+localStorage.setItem("players", userName)
+    // data.forEach(element => {
+    //     let player = document.createElement("li");
+    //     player.textContent = element.name;
+    //     allPlayers.appendChild(player)
+    // }
+    //    );
+   
 
-
-
-//   let inter = localStorage.getItem("1");
-//   let array = JSON.parse(inter);
-//   userName = inputUser.value;
-//   (player.name = userName), (player.totalScore = 0);
-//   array.push(player)
-//   console.log(array);
-//   let stringData = JSON.stringify(array);
-//   localStorage.setItem("1", stringData);
 });
+
+// userName = inputUser.value;
+//     (player.name = userName), (player.totalScore = 0);
+  
+//     players = JSON.stringify(player);
+//     localStorage.setItem("name", players);
+//     inputUser.value = "";
