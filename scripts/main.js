2033874a3c573276c1895b2ba86a37805566a6ae
reduansj <reduansenoun@gmail.com>
2022-04-26 17:06:15 +0200

    feature: javascript game page

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,80 @@
-const UsersData = {name:"", totalScore:0};
+import { cardData } from "./utilites.js";
+const cardItem = document.querySelectorAll(".card-box");
+const gameSection = document.getElementById("cardWrapper");
+let i = 0;
+let clickCounter = 0;
+let cardNames = [];
+let matches = 8;
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  getCardData(cardData, cardItem);
+});
+
+const getCardData = (cardData, cardItem) => {
+  cardData.sort(() => Math.random() - 0.5);
+
+  cardItem.forEach((cardContainer) => {
+    //add id and name to image container
+    cardContainer.id = cardData[i].id;
+    cardContainer.setAttribute("name", cardData[i].name);
+    //creat an image inside div container
+    const cardImage = document.createElement("img");
+    cardImage.src = cardData[i].imgSrc;
+    cardImage.classList = "front";
+
+    //creat the reverse image
+    const cardReverse = document.createElement("img");
+    cardReverse.src = "./assets/reverse.png";
+    cardReverse.classList = "reverse";
+
+    cardContainer.appendChild(cardImage);
+    cardContainer.appendChild(cardReverse);
+    i++;
+
+    cardContainer.addEventListener("click", (e) => {
+      cardImage.classList.toggle("toggle");
+      cardContainer.classList.toggle("toggle");
+      checkCards(cardContainer);
+    });
+  });
+};
+
+function checkCards(card) {
+  clickCounter++;
+  cardNames.push(card);
+  card.style.pointerEvents = "none";
+
+  if (clickCounter == 2) {
+    gameSection.style.pointerEvents = "none";
+    const firstCardName = document.getElementById(`${cardNames[0].id}`);
+    const secondCardName = document.getElementById(`${cardNames[1].id}`);
+
+    console.log(firstCardName.name);
+
+    if (
+      firstCardName.getAttribute("name") === secondCardName.getAttribute("name")
+    ) {
+      firstCardName.style.pointerEvents = "none";
+      secondCardName.style.pointerEvents = "none";
+      gameSection.style.pointerEvents = "auto";
+      matches--;
+      cardNames = [];
+      clickCounter = 0;
+      if (matches == 0) {
+        console.log("copion");
+      }
+    } else {
+      firstCardName.style.pointerEvents = "auto";
+      secondCardName.style.pointerEvents = "auto";
+      setTimeout(() => {
+        firstCardName.classList.toggle("toggle");
+        secondCardName.classList.toggle("toggle");
+        firstCardName.firstChild.classList.toggle("toggle");
+        secondCardName.firstChild.classList.toggle("toggle");
+        gameSection.style.pointerEvents = "auto";
+        cardNames = [];
+        clickCounter = 0;
+      }, 1000);
+    }
+  }
+}
---
+++ scripts/utilites.js
@@ -0,0 +1,20 @@
+const cardData = [
+  { imgSrc: "./assets/1.png", id: "cardOne", name: "one" },
+  { imgSrc: "./assets/2.png", id: "cardTwo", name: "two" },
+  { imgSrc: "./assets/3.png", id: "cardThree", name: "three" },
+  { imgSrc: "./assets/4.png", id: "cardFour", name: "four" },
+  { imgSrc: "./assets/5.png", id: "cardFive", name: "five" },
+  { imgSrc: "./assets/6.png", id: "cardSix", name: "six" },
+  { imgSrc: "./assets/7.png", id: "cardSeven", name: "seven" },
+  { imgSrc: "./assets/8.png", id: "cardEight", name: "eight" },
+  { imgSrc: "./assets/1.png", id: "cardNine", name: "one" },
+  { imgSrc: "./assets/2.png", id: "cardTen", name: "two" },
+  { imgSrc: "./assets/3.png", id: "cardEleven", name: "three" },
+  { imgSrc: "./assets/4.png", id: "cardTwelve", name: "four" },
+  { imgSrc: "./assets/5.png", id: "cardThirteen", name: "five" },
+  { imgSrc: "./assets/6.png", id: "cardFourteen", name: "six" },
+  { imgSrc: "./assets/7.png", id: "cardFiveteen", name: "seven" },
+  { imgSrc: "./assets/8.png", id: "cardSixteen", name: "eight" },
+];
+
+export { cardData };
